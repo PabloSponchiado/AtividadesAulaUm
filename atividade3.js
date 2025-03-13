78c915b4aa978f4f84a13f00f401d689375b398d
Pablo Isaac Sponchiado da Silva <pablostz2019@gmail.com>
2025-03-13 16:19:51 -0300

    Delete atividade3.js

--- atividade3.js
+++
@@ -1,8 +0,0 @@
-var teclado = require("prompt-sync")();
-console.log("-----------------------------------------");
-console.log("--Convers\u00E3o de CELSIUS para FAHRENHEIT---");
-console.log("-----------------------------------------");
-var tempc = parseInt(teclado("digite a temperatura em celsius: "));
-var tempf = 0;
-tempf = (tempc * (9 / 5)) + 32;
-console.log("a temperatura em FAHRENHEIT:".concat(tempf));
